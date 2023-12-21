fe4e61e7275ad5d0859fffed6269ce9dcc049f95
AlexPhoenix45 <33224969+AlexPhoenix45@users.noreply.github.com>
2023-12-21 17:38:47 +0700

    change table from canvas to gameobject

--- PhuCDTGames_Ludopoly/Assets/PhuCDTGames_Assets/Scripts/Slot.cs
+++ PhuCDTGames_Ludopoly/Assets/PhuCDTGames_Assets/Scripts/Slot.cs
@@ -26,20 +26,23 @@ public class Slot : MonoBehaviour
     [Header("Color Property")]
     [Header("Local Attribute")]
     public GameObject colorProperty_Panel;
-    public TextMeshProUGUI colorProperty_Panel_Name;
-    public TextMeshProUGUI colorProperty_Panel_Price;
+    public GameObject colorProperty_Panel_Name;
+    public GameObject colorProperty_Panel_Price;
 
     [Header("Special Property")]
     public GameObject specialProperty_Panel;
-    public TextMeshProUGUI specialProperty_Panel_Name;
-    public TextMeshProUGUI specialProperty_Panel_Price;
+    public GameObject specialProperty_Panel_Name;
+    public GameObject specialProperty_Panel_Price;
     public GameObject specialProperty_Panel_Image;
 
     [Header("Suprise Slot")]
     public GameObject supriseSlot_Panel;
-    public TextMeshProUGUI supriseSlot_Panel_Name;
+    public GameObject supriseSlot_Panel_Name;
+    public GameObject supriseSlot_Panel_Price; //This is for only tax slot
     public GameObject supriseSlot_Panel_Image;
-    public TextMeshProUGUI supriseSlot_Panel_Price; //This is for only tax slot
+
+    [Header("Corner Slot")]
+    public GameObject cornerSlot_Text;
 
     private void Start()
     {
@@ -50,32 +53,51 @@ public class Slot : MonoBehaviour
     {
         if (slotType == Type.ColorProperty)
         {
-            gameObject.GetComponent<Image>().sprite = colorProperty.propertyImage;
+            gameObject.GetComponent<SpriteRenderer>().sprite = colorProperty.propertyImage;
             colorProperty_Panel.SetActive(true);
-            colorProperty_Panel_Name.text = colorProperty.propertyName.ToUpper();
-            colorProperty_Panel_Price.text = colorProperty.propertyPrice.ToString() + "$";
+            colorProperty_Panel_Name.GetComponent<TextMeshPro>().text = colorProperty.propertyName.ToUpper();
+            colorProperty_Panel_Price.GetComponent<TextMeshPro>().text = colorProperty.propertyPrice.ToString() + "$";
         }
         else if (slotType == Type.SpecialProperty)
         {
             specialProperty_Panel.SetActive(true);
-            specialProperty_Panel_Name.text = specialProperty.propertyName.ToUpper();
-            specialProperty_Panel_Price.text = specialProperty.propertyPrice.ToString() + "$";
-            specialProperty_Panel_Image.gameObject.GetComponent<Image>().sprite = specialProperty.propertyImage;
+            specialProperty_Panel_Name.GetComponent<TextMeshPro>().text = specialProperty.propertyName.ToUpper();
+            specialProperty_Panel_Price.GetComponent<TextMeshPro>().text = specialProperty.propertyPrice.ToString() + "$";
+            specialProperty_Panel_Image.gameObject.GetComponent<SpriteRenderer>().sprite = specialProperty.propertyImage;
         }
         else if (slotType == Type.SupriseSlot)
         {
             supriseSlot_Panel.SetActive(true);
-            supriseSlot_Panel_Name.text = supriseSlot.slotName.ToUpper();
-            supriseSlot_Panel_Image.gameObject.GetComponent<Image>().sprite = supriseSlot.slotImage;
+            supriseSlot_Panel_Name.GetComponent<TextMeshPro>().text = supriseSlot.slotName.ToUpper();
             if (supriseSlot.taxPrice > 0)
             {
-                supriseSlot_Panel_Price.text = "PAY " + supriseSlot.taxPrice.ToString() + "$";
+                supriseSlot_Panel_Price.GetComponent<TextMeshPro>().text = "PAY " + supriseSlot.taxPrice.ToString() + "$";
             }
             else
             {
-                supriseSlot_Panel_Price.text = "";
+                supriseSlot_Panel_Price.GetComponent<TextMeshPro>().text = "";
+            }
+            supriseSlot_Panel_Image.gameObject.GetComponent<SpriteRenderer>().sprite = supriseSlot.slotImage;
+        }
+        else if (slotType == Type.CornerSlot)
+        {
+            cornerSlot_Text.SetActive(true);
+            if (cornerSlot.slotType == CornerSlot.Type.Go)
+            {
+                cornerSlot_Text.GetComponent<TextMeshPro>().text = "GO";
+            }
+            else if (cornerSlot.slotType == CornerSlot.Type.VisitingJail)
+            {
+                cornerSlot_Text.GetComponent<TextMeshPro>().text = "JAIL";
+            }
+            else if (cornerSlot.slotType == CornerSlot.Type.Parking)
+            {
+                cornerSlot_Text.GetComponent<TextMeshPro>().text = "PARK LOT";
+            }
+            else if (cornerSlot.slotType == CornerSlot.Type.GoToJail)
+            {
+                cornerSlot_Text.GetComponent<TextMeshPro>().text = "GO TO JAIL";
             }
         }
-
     }
 }
---
+++ PhuCDTGames_Ludopoly/Assets/PhuCDTGames_Assets/Scripts/Table.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Table : MonoBehaviour
+{
+    public GameObject[] slot;
+    public GameObject[] player;
+
+    private void Start()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+
+    }
+
+
+}
